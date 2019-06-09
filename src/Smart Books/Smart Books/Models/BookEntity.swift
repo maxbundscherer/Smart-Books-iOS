d50c052540ac1177267d535ab45ce10afd5bfd6a
Maximilian Bundscherer <maximilian@bundscherer-online.de>
2019-06-09 12:38:28 +0200

    Add BookEntity - closes #6

---
+++ src/Smart Books/Smart Books/Models/BookEntity.swift
@@ -0,0 +1,27 @@
+//
+//  BookEntity.swift
+//  Smart Books
+//
+//  Created by Maximilian Bundscherer on 09.06.19.
+//  Copyright © 2019 Maximilian Bundscherer. All rights reserved.
+//
+
+import UIKit
+
+class BookEntity {
+ 
+    var title: String?
+    var isbn: String?
+    var publisher: String?
+    var tags: [String]
+    var coverImage: UIImage?
+    
+    init(title: String?, isbn: String?, publisher: String?, tags: [String], coverImage: UIImage?) {
+        self.title = title
+        self.isbn = isbn
+        self.publisher = publisher
+        self.tags = tags
+        self.coverImage = coverImage
+    }
+    
+}
